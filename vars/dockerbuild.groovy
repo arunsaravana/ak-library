6a8f180516a1ec15846d09585b2611d242965833
Arunkumar P S <arunkumar96293@gmail.com>
2020-05-29 17:55:26 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -13,8 +13,8 @@ withCredentials([usernamePassword(
        
     
 }   
-            def image= "${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"
-            echo "${image}" 
-            sh 'sed -i s/"IMAGEID"/"$image"/g  app-deployment.yaml'          
+            def imageid= "${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"
+            echo "${imageid}" 
+            sh 'sed -i s/"IMAGEID"/"$imageid"/g  app-deployment.yaml'          
     //sh 'sed -i s/"IMAGEID"/"${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"/g  app-deployment.yaml'
 }
