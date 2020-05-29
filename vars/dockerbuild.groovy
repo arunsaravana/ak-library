fe91728838488e8fbc707e9e81b8df4ee3216670
Arunkumar P S <arunkumar96293@gmail.com>
2020-05-29 17:52:03 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -5,14 +5,16 @@ withCredentials([usernamePassword(
             usernameVariable: "Username",
             passwordVariable: "Password"
         )]) {
-    def image= "${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"
-     echo "${image}"
+   
     sh "docker login -u '$Username' -p '$Password'"
     sh "docker image build -t ${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER} ."
     sh "docker image push ${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"
     sh "docker image rm ${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"   
-    sh 'sed -i s/"IMAGEID"/"${image}"/g  app-deployment.yaml'        
+       
     
-}    
+}   
+            def image= "${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"
+            echo "${image}" 
+            sh 'sed -i s/"IMAGEID"/"${image}"/g  app-deployment.yaml'          
     //sh 'sed -i s/"IMAGEID"/"${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"/g  app-deployment.yaml'
 }
