a24cfd5554467761887e220de340f7542f5cecff
Arunkumar P S <arunkumar96293@gmail.com>
2020-05-29 18:01:27 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,5 +1,4 @@
 def call(String hubuser, String repo, String repotag, String hubcred) {
-           def imagebuild= "${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"
 withCredentials([usernamePassword(
             credentialsId: "${hubcred}",
             usernameVariable: "Username",
@@ -10,8 +9,6 @@ withCredentials([usernamePassword(
     sh "docker image build -t ${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER} ."
     sh "docker image push ${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"
     sh "docker image rm ${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"   
-    echo "${imagebuild}" 
-    sh 'sed -i s/"IMAGEID"/"$imagebuild"/g  app-deployment.yaml' 
+ 
     }                    
-    //sh 'sed -i s/"IMAGEID"/"${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"/g  app-deployment.yaml'
 }
