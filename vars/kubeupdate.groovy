eac062c98f85389b5f8b6946e5017ca950e3b2dc
Arunkumar P S <arunkumar96293@gmail.com>
2020-05-29 18:01:40 +0530

    Update kubeupdate.groovy

--- vars/kubeupdate.groovy
+++ vars/kubeupdate.groovy
@@ -7,7 +7,7 @@ secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']])
  {
               echo "Login Successfull"
               sh "aws eks --region '${region}'  update-kubeconfig --name '${ekscluster}'"
-            //  sh 'sed -i s/"BUILD_NUMBER"/"v$BUILD_NUMBER"/g app-deployment.yaml'
+              sh 'sed -i s/"BUILD_NUMBER"/"v$BUILD_NUMBER"/g app-deployment.yaml'
               sh 'kubectl apply -f app-deployment.yaml'
               sh 'kubectl apply -f app-service.yaml'
               sh 'kubectl get svc'
