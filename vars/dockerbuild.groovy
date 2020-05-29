d932a0d389a34d849b3ca912d0551ee022354a7c
Arunkumar P S <arunkumar96293@gmail.com>
2020-05-29 15:47:07 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,7 +1,12 @@
-def call(String hubuser, String repo, String repotag) {
-
+def call(String hubuser, String repo, String repotag, String awscred) {
+withCredentials([usernamePassword(
+            credentialsId: ${awscred},
+            usernameVariable: "Username",
+            passwordVariable: "Password"
+        )]) {
     sh "docker login -u '$Username' -p '$Password'"
     sh "docker image build -t ${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER} ."
     sh "docker image push ${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"
     sh "docker image rm ${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"   
+}
 }
